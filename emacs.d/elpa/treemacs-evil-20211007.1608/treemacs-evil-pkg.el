b968742afc8ac10872fa2756e2b56e085a36f09e
deglaus <johan.visteby@gmail.com>
2022-11-27 14:59:49 +0100

    added latex snippets!

--- emacs.d/elpa/treemacs-evil-20211007.1608/treemacs-evil-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from treemacs-evil.el  -*- no-byte-compile: t -*-
-(define-package "treemacs-evil" "20211007.1608" "Evil mode integration for treemacs" '((emacs "26.1") (evil "1.2.12") (treemacs "0.0")) :commit "935ee46b67e651b77569d5e94162a0eabb6ea300" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
